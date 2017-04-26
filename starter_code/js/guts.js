84db74941725377a88471871ba2c59b0bd2b34a7
Ian Bueno <ian.b.student@lyonscommunityschool.org>
2017-04-26 14:26:27 +0000

    Earl and Ian

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -1,35 +1,46 @@
+//Ian and Earl's Calculator
 // All the code you will need to create for this project will be completed below
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
 
+
 function divide(x,y) {
 	//Write your code below this line
-
+ var bye = x/y;
+ return bye;
 }
 
 function multiply(x,y) {
-
+var product = x*y;
+return product;
 }
 
 function subtract(x,y) {
+    var dink = x-y;
+    return dink;
 
 }
 
 function add(x,y) {
+    var sum = x+y;
+    return sum;
 
 }
 
 function power(x,y) {
-
+var sky = Math.pow(x,y);
+return sky;
 }
 
 function square_root(x) {
-
+//var guy = √x = y;
+//return guy;
 }
 
 function logBase10(x) {
-
+var dem = Math.log(x);
+return dem;
 }
 
 function generateRandomNumber() {
